81a581f49674519d4f156075317f9647e44e5210
William Coutinho <william.csouza@hotmail.com>
2023-09-21 15:01:56 -0300

    Domain model class for each feature

---
+++ src/main/java/coutinho/santanderbootcamp/domain/model/Account.java
@@ -0,0 +1,65 @@
+package coutinho.santanderbootcamp.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false) // Constraint
+    private String number;
+
+    @Column(nullable = false)
+    private String agency;
+
+    @Column(scale = 13, precision = 2, nullable = false)// 11 dígitos com 2 casas decimais
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", scale = 13, precision = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
