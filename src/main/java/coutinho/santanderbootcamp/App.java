3be33e5e7c0a59cd28accc1e42c11651d7a15172
William Coutinho <william.csouza@hotmail.com>
2023-09-24 00:05:01 -0300

    Bugfix CORS

--- src/main/java/coutinho/santanderbootcamp/App.java
+++ src/main/java/coutinho/santanderbootcamp/App.java
@@ -1,8 +1,13 @@
 package coutinho.santanderbootcamp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Resolução de CORS
+//https://stackoverflow.com/questions/70843940/springdoc-openapi-ui-how-do-i-set-the-request-to-https/71132608#71132608
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class App {
 
